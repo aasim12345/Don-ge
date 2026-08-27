b7265b969988645bd66e943c8143220239760fc1
Aasim Hussain <9957924@schoolsnet.act.edu.au>
2026-08-27 13:43:53 +1000

    web

--- Don@ge/Don_ge/urls.py
+++ Don@ge/Don_ge/urls.py
@@ -8,5 +8,6 @@ from django.contrib import admin
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('main.urls')),
+  
 ]
    
--- Don@ge/main/views.py
+++ Don@ge/main/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 def home(request):
     return render(request, 'main/index.html')
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'main/signup.html', {'form': form})
+
+
+def login_view(request):
+    error = None
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = 'Incorrect username or password.'
+
+    return render(request, 'main/login.html', {'error': error})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
